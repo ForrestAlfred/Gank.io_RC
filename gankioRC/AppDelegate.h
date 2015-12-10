ef63167e7bdb1517c397eb517d3a7ab1cd65ecd5
Prolospro <273481701@qq.com>
2015-12-10 11:01:16 +0800

    Initial Commit

---
+++ gankioRC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gankioRC
+//
+//  Created by 徐腾浩 on 15/12/10.
+//  Copyright © 2015年 iOSPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gankioRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gankioRC
+//
+//  Created by 徐腾浩 on 15/12/10.
+//  Copyright © 2015年 iOSPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
